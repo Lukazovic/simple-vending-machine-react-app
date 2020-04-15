90fb2b05340900edc053d5f0dceac3b3e8892c9c
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-04-14 23:18:51 -0300

    Adding react router

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -6,12 +6,16 @@ export default class VendingMachine extends Component {
   render() {
     return (
       <div className="VendingMachine">
-        <h2 className="VendingMachine-title">What would you like?</h2>
-        <img src={vendingMachineImage} alt="Vending machine" />
-        <div className="VendingMachine-options">
-          <h2>Chips</h2>
-          <h2>Fanta</h2>
-          <h2>Snickers</h2>
+        <div className="VendingMachine-container">
+          <h2 className="VendingMachine-container-title">
+            What would you like?
+          </h2>
+          <img src={vendingMachineImage} alt="Vending machine" />
+          <div className="VendingMachine-container-options">
+            <h2>Chips</h2>
+            <h2>Fanta</h2>
+            <h2>Snickers</h2>
+          </div>
         </div>
       </div>
     );
