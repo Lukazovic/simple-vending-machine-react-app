87a17b536ee1d57504ddc1af596b23a3cd26c3b4
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-04-14 23:55:54 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import VendingMachine from "./VendingMachine";
+import Chips from "./Chips";
+import Soda from "./Soda";
 
 function App() {
   return (
     <div className="App">
-      <VendingMachine />
+      {/* <VendingMachine /> */}
+      {/* <Chips /> */}
+      <Soda />
     </div>
   );
 }
